eaddd8176bb758a1db6cd23bfb50428ea45fccd7
ChristianSama <christiansama1601@gmail.com>
2022-02-26 20:52:49 -0500

    cached images in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import Scoreboard from './Scoreboard';
 import Gallery from './Gallery'
-// import './App.css';
 
 function App() {
   const [curScore, setCurScore] = useState(0);
@@ -10,16 +9,22 @@ function App() {
   const [correctTries, setCorrectTries] = useState([]);
 
   useEffect(() => {
-    fetchChars();
+    getChars();
   }, [])
 
-  const fetchChars = async () => {
+  const getChars = async () => {
     const maxPages = 42;
     const randomPage = Math.floor(Math.random() * maxPages) + 1;
     const url = 'https://rickandmortyapi.com/api/character?page=' + randomPage;
     try {
       const response = await fetch(url);
       const json = await response.json();
+
+      //cache images
+      for (let char of json.results) {
+        let img = new Image();
+        img.src = char.image;
+      }
       setCharacters(json.results);
 
     } catch (error) {
@@ -45,25 +50,12 @@ function App() {
     }
   }
 
-  //get random sample of a particular size
-  const gallerySize = 12;
-  const galleryChars = shuffle([...characters]).slice(0, gallerySize);
-
   return (
     <div>
       <Scoreboard curScore={curScore} bestScore={bestScore}></Scoreboard>
-      <Gallery handleClick={handleImageClick} characters={galleryChars}></Gallery>
+      <Gallery handleClick={handleImageClick} characters={characters}></Gallery>
     </div>
   );
-}
-
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
-
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
 }
 
 export default App;
--- src/Gallery.js
+++ src/Gallery.js
@@ -1,16 +1,27 @@
-import uniqid from 'uniqid';
-
 function Gallery(props) {
-  const characters = props.characters.map((char) => {
-    const key = uniqid();
+  //get random sample of a particular size
+  const gallerySize = 12;
+  const characters = shuffle(...props.characters).slice(0, gallerySize);
+
+  const galleryChars = characters.map((char) => {
     return <img onClick={props.handleClick} src={char.image} key={char.id} id={char.id}/>
   });
 
   return (
     <div className="gallery">
-      {characters}
+      {galleryChars}
     </div>
   );
 }
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 
 export default Gallery;
