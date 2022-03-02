c43f793d80826b8f618d3372d72590ef0b8f56c6
ChristianSama <christiansama1601@gmail.com>
2022-03-01 19:44:53 -0500

    added modal hidding on click outside

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -3,16 +3,19 @@ import '../css/Modal.css';
 function Modal(props) {
 
   const hideModal = (event) => {
-    const modal = document.querySelector('.modal');
-    modal.style.display = "none";
+    let target = event.target;
+    if (target.classList.contains("modal") || target.id === "modal-btn") {
+      const modal = document.querySelector(".modal");
+      modal.style.display = "none";
+    }
   }
 
   return(
-    <div className="modal">
+    <div onClick={hideModal} className="modal">
       <div className="window">
         <h3>Instructions</h3>
         <p>Click on the most number of cards without clicking on the same card twice.</p>
-        <button onClick={hideModal}>Continue</button>
+        <button id="modal-btn" onClick={hideModal}>Continue</button>
       </div>
     </div>
   );
